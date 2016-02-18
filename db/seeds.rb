2815c9538bbb1258cfb799e40a92fcbbd01e9f25
rjjq <rrrjjjqqq@gmail.com>
2016-02-18 23:14:14 +0800

    撰寫db:seed, 自動化重整資料庫(chap10)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.new
+user.email = "test@gmail.com"
+user.name = "tester"
+user.password = "12345678"
+user.password_confirmation = "12345678"
+user.save
+
+1.upto(20) do |i|
+  group = Group.new
+  group.title = "Group no.#{i}"
+  group.description = "第#{i}個討論版"
+  group.user_id = user.id
+  group.save
+
+  user.join!(group)
+
+  1.upto(30) do |k|
+    Post.create!([group_id: "#{i}", content: "第#{k}個留言", user_id: user.id])
+  end
+end
